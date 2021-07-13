658a28b986119eaa76dae60db67b6db49ad3c2ba
Emanuel Medeiros Claudino <48090017+emanuel1222@users.noreply.github.com>
2021-07-13 19:54:19 -0300

    Add files via upload

---
+++ ImagemMetodoFabrica/src/metodo/fabrica/MetodoFabrica.java
@@ -0,0 +1,22 @@
+//Emanuel medeiros - POO2 2021/2 - 20171BSI0146
+package metodo.fabrica;
+
+
+public class MetodoFabrica {
+
+    public static void main(String[] args) {
+        Formato formato;
+        formato = FabricaFormato.gerarFormato("jpeg");
+        formato.gera();
+        
+        formato = FabricaFormato.gerarFormato("png");
+        formato.gera();
+
+        formato = FabricaFormato.gerarFormato("gif");
+        formato.gera();
+
+        formato = FabricaFormato.gerarFormato("bmp");
+        formato.gera();
+    }    
+}
+
