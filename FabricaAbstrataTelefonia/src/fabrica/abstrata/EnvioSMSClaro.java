96540dec56c33adc62d1fd5f0a57e6264a89dfa5
Emanuel Medeiros Claudino <48090017+emanuel1222@users.noreply.github.com>
2021-07-31 18:46:02 -0300

    Add files via upload

---
+++ FabricaAbstrataTelefonia/src/fabrica/abstrata/EnvioSMSClaro.java
@@ -0,0 +1,12 @@
+package fabrica.abstrata;
+
+public class EnvioSMSClaro implements EnvioSMS{
+
+    @Override
+    public boolean enviar (Mensagem msg, int telefone) {
+        System.out.println("Enviando mensagem:" + msg.texto + 
+        "para o telefone Claro:" + telefone);
+
+        return true;
+    }
+}
---
+++ FabricaAbstrataTelefonia/src/fabrica/abstrata/FabricaClaro.java
@@ -0,0 +1,14 @@
+package fabrica.abstrata;
+
+public class FabricaClaro extends FabricaAbstrataEnvios{
+
+    @Override
+    public Cobranca criaCobranca(){
+        return new CobrancaClaro();
+    }
+    
+    @Override
+    public EnvioSMS criaEnvioSMS(){
+        return new EnvioSMSClaro();
+    }
+}
